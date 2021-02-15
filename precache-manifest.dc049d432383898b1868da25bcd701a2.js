0e2f131198c2a995308f0d4fb40b790780ade2b2
Olakunle Salami <kunsal2003@gmail.com>
2021-02-15 16:40:59 +0100

    Updates

---
+++ precache-manifest.dc049d432383898b1868da25bcd701a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b73a8531051d16b9d51a16ba802929",
+    "url": "/kaytivity/index.html"
+  },
+  {
+    "revision": "2990f48b70f2c25ed5df",
+    "url": "/kaytivity/static/css/main.d2b7be9c.chunk.css"
+  },
+  {
+    "revision": "3317de616c41dd3f9d7a",
+    "url": "/kaytivity/static/js/2.94f4e732.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kaytivity/static/js/2.94f4e732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2990f48b70f2c25ed5df",
+    "url": "/kaytivity/static/js/main.383da8a9.chunk.js"
+  },
+  {
+    "revision": "b44f43ce78178d271ada",
+    "url": "/kaytivity/static/js/runtime-main.5451a41b.js"
+  },
+  {
+    "revision": "05a25aab39f956013cf1f684b59d7642",
+    "url": "/kaytivity/static/media/kunle2.05a25aab.jpg"
+  },
+  {
+    "revision": "bec4677b67d5ba55ea5a744ad0466011",
+    "url": "/kaytivity/static/media/macbook-code.bec4677b.jpg"
+  }
+]);
